d2f9cd8aff67cbc400171c375f73b3aa5d898422
Diane Woodbridge <dwoodbridge@usfca.edu>
2025-08-25 09:31:54 -0700

    Added Day2/Skeleton

---
+++ Day2/Skeleton/ex01.sh
@@ -0,0 +1,3 @@
+# 1. On feature/search branch, make some changes and check where main and feature/search branch are currently at.
+
+# 2. Switch to the main branch, make some changes and check  the main and feature/search branch are currently at.
